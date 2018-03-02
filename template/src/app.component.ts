516b725d0dc0d6002f6a2fc5a98b922fcf19cb06
Jason M Penniman <jason.penniman@milestonetg.com>
2018-03-02 10:19:46 -0500

    Added template

---
+++ template/src/app.component.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'App',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('component initialized');
+  }
+}
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
